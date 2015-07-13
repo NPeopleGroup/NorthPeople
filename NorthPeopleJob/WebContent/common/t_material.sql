a00f4296e4ecb526248dd5ca2eeaf5bf44b7f917
xuwaye <10618830@qq.com>
2015-07-13 09:11:11 +0800

    徐伟 在Question表又增加了A-F 6个所需文件的字段

--- NorthPeopleJob/WebContent/common/t_material.sql
+++ NorthPeopleJob/WebContent/common/t_material.sql
@@ -30,6 +30,12 @@ CREATE TABLE `t_material` (
 
 /*
 徐伟 Date: 2015-07-10 16:23:00
-*/
+注意！！！ 语句要逐条运行
+**/
 ALTER TABLE  `t_question` ADD  `isFirst` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT  '是本分类的第一个问题' AFTER  `type`
-
+ALTER TABLE  `t_question` ADD  `fileA2` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT  '文件A2' AFTER  `fileA`
+ALTER TABLE  `t_question` ADD  `fileB2` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT  '文件B2' AFTER  `fileB`
+ALTER TABLE  `t_question` ADD  `fileC2` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT  '文件C2' AFTER  `fileC`
+ALTER TABLE  `t_question` ADD  `fileD2` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT  '文件D2' AFTER  `fileD`
+ALTER TABLE  `t_question` ADD  `fileE2` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT  '文件E2' AFTER  `fileE`
+ALTER TABLE  `t_question` ADD  `fileF2` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT  '文件F2' AFTER  `fileF`
